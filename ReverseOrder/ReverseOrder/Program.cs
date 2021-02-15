3216cbad9eb3b1fad374554e888e53f05d04ed20
alif2543 <alzawedalif@gmail.com>
2021-02-15 17:07:43 +0600

    lab task 2

---
+++ ReverseOrder/ReverseOrder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 3, 5, 6, 7 };
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("     The Numbers in Reverse Order are: ");
+            for (int i = arr1.Length; i > 0; i--)
+            {
+                Console.Write(arr1[i - 1] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            
+        }
+    }
+}
---
+++ copyarray/copyarray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace copyarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 2,3,5,7,8,9};
+            int[] arr2 = new int[5];
+            for (int k = 0; k < arr1.Length; k++)
+            {
+                arr2[k] = arr1[k];
+            }
+            for (int l = 0; l < arr2.Length; l++)
+            {
+                Console.Write(arr2[l] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
