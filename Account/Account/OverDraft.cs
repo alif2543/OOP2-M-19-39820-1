94e43c282d9f66b0f4ef0964e17d1e2b73fe5aea
alif2543 <alzawedalif@gmail.com>
2021-02-22 19:51:52 +0600

    lab task account

---
+++ Account/Account/OverDraft.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+    class OverDraft
+    {
+        private int limit;
+
+        public int Limit
+        {
+            get { return limit; }
+            set { limit = value; }
+        }
+
+    }
+}
