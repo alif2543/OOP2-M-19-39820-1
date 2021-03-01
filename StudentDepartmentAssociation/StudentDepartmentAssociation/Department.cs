9210d5ff0898d16816109ca93c2be5d06eae34df
alif2543 <alzawedalif@gmail.com>
2021-03-01 11:27:20 +0600

    Student Dept Association lab task

---
+++ StudentDepartmentAssociation/StudentDepartmentAssociation/Department.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDepartmentAssociation
+{
+    class Department
+    {
+        private string id;
+        private string name;
+
+        private int studentCount;
+        private Student[] students;
+
+        public Department()
+        {
+            students = new Student[50];
+        }
+
+        public Department(string id, string name)
+        {
+            this.Name = name;
+            this.Id = id;
+            students = new Student[50];
+        }
+
+        public string Id
+        {
+            set { this.id = value; }
+            get { return this.id; }
+        }
+
+        public string Name
+        {
+            set { this.name = value; }
+            get { return this.name; }
+        }
+
+        public int StudentCount
+        {
+            set { this.studentCount = value; }
+            get { return this.studentCount; }
+        }
+
+        public void AddStudent(params Student[] stdns)
+        {
+            foreach (Student s in stdns)
+            {
+                this.students[this.StudentCount++] = s;
+                s.Dept = this;
+            }
+        }
+
+        public void RemoveStudent(Student s)
+        {
+            if (s == this.students[this.StudentCount])
+            {
+                this.students[this.StudentCount--] = null;
+                return;
+            }
+
+            bool notFound = true;
+            for (int i = 0; i < this.StudentCount - 1; ++i)
+            {
+                if (s == this.students[i] && notFound)
+                {
+                    this.students[i] = null;
+                    notFound = false;
+                }
+
+                if (!notFound)
+                    this.students[i] = this.students[i + 1];
+
+            }
+        }
+
+        public Student GetStudent(string id)
+        {
+            for (int i = 0; i < this.StudentCount; ++i)
+                if (id == this.students[i].Id)
+                    return this.students[i];
+
+            return null;
+        }
+        
+        
+        
+        
+        public void PrintStudent()
+        {
+            foreach (Student s in this.students)
+            {
+                s.ShowInfo();
+            }
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("DEPARTMENT ID: {0}", this.Id);
+            Console.WriteLine("DEPARTMENT NAME: {0}", this.Name);
+        }
+    }
+}
---
+++ StudentDepartmentAssociation/StudentDepartmentAssociation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDepartmentAssociation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("4946", "ALIF", 2.8F);
+            Student s2 = new Student("5923", "ALFI", 3.5F);
+            Student s3 = new Student("498565", "ROZA", 3.1F);
+
+            Department d1 = new Department("CS", "COMPUTER SCIENCE");
+            Department d2 = new Department("EEE", "ELECTRICAL AND ELECTRONIXS ENGINEERING");
+
+            s1.Dept = d1;
+            s2.Dept = d2;
+            s3.Dept = d2;
+
+            s1.ShowInfo();
+            s1.ShowDepartment();
+
+            s2.ShowInfo();
+            s2.ShowDepartment();
+
+            s3.ShowInfo();
+            s3.ShowDepartment();
+
+            d1.Name = "COMPUTER SCIENCE AND ENGINEERING";
+
+            s1.ShowInfo();
+            s1.ShowDepartment();
+
+            d1.AddStudent(s1);
+            d1.AddStudent(s2);
+
+            
+            
+            d1.PrintStudent();
+        }
+    }
+}
